c7b76bfbb5d330085f1a144210c9b7927013d84c
lingfeilol <lingfeilol@163.com>
2020-07-16 23:59:12 +0800

    300. 最长上升子序列

---
+++ ac/dp/ac96.c
@@ -0,0 +1,18 @@
+int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+1);
+        int ret=0;
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=1;
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+            ret=max(ret,dp[i]);
+        }
+        return ret;
+    }
